f94a3dfeace31ecebe14d7bf2c441341b86b4fe5
AmelRodr <amelrodr@gmail.com>
2018-09-26 00:52:17 -0500

    Edit Users done

--- routes/providers.js
+++ routes/providers.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const User = require('../models/User')
 const passport = require('passport');
+const uploadCloud = require('../helpers/cloudinary')
 
 //Profile
 router.get('/:username', (req, res, next)=>{
@@ -12,6 +13,28 @@ router.get('/:username', (req, res, next)=>{
       console.log(error)
     })
 })
+
+
+router.get('/:username/edit',(req,res,next)=>{
+  const {username} = req.params
+  console.log(req.user._id)
+  User.findById(req.user._id) 
+  .then(user=>{
+    res.render('providers/edit',user)
+  }).catch(e=>next(e))
+})/*
+
+router.post('/:username/edit',uploadCloud.single('image'),(req,res,next)=>{
+  const {username} = req.params
+  if(req.file) req.body['photoURL'] = req.file.url
+  User.findOneAndUpdate({username:username},{$set:req.body},{new:true})
+  .then(user=>{
+    res.redirect(`/providers/${username}`)
+  }).catch(e=>{
+    console.log(e)
+  })
+})                    
+*/
 
 
 module.exports = router
--- routes/users.js
+++ routes/users.js
@@ -7,10 +7,34 @@ router.get('/:username', (req, res, next)=>{
   const {username} = req.params
   User.findOne({username:username})
     .then(user=>{    
-      res.render('users/profile', user)
+      res.render('users/profile',user)
     }).catch(error=>{
       console.log(error)
     })
 })
+
+
+///EDIT
+
+router.get('/:username/edit',(req,res,next)=>{
+  const {username} = req.params
+  console.log(req.user._id)
+  User.findById(req.user._id) 
+  .then(user=>{
+    res.render('users/edit',user)
+  }).catch(e=>next(e))
+})
+
+router.post('/:username/edit',uploadCloud.single('image'),(req,res,next)=>{
+  const {username} = req.params
+  if(req.file) req.body['photoURL'] = req.file.url
+  User.findOneAndUpdate({username:username},{$set:req.body},{new:true})
+  .then(user=>{
+    res.redirect(`/users/${username}`)
+  }).catch(e=>{
+    console.log(e)
+    console.log
+  })
+})                    
 
 module.exports = router 
